5d977906a01971a0aebbe42751e6248baaf2f661
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2019-03-02 03:13:37 +0530

    fix tslint errors

--- management/src/app/signup-login/signup/signup.component.ts
+++ management/src/app/signup-login/signup/signup.component.ts
@@ -178,14 +178,11 @@ export class SignupComponent implements OnInit {
 
         this.showSpinner = false;
 
-        if (error1.code == 'auth/email-already-in-use')
-
+        if(error1.code === 'auth/email-already-in-use') {
           this.state = this.signupStates[0];
-
-        else
-
+        } else {
           this.state = this.signupStates[1];
-
+        }
       }
 
     );
