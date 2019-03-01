6441bd457f2b6ea9d8df1dce61ade3e663517319
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2019-03-01 23:35:05 +0530

    add route module

--- management/src/app/app-routing.module.ts
+++ management/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: '/presentations', pathMatch: 'full'},
+  {path: 'login', component:LoginComponent},
+  {path: 'signUp', component:SignUpComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+
+  declarations: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
