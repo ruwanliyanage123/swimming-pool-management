915cd4c9c52d2930419e164351480f241655ba39
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2019-03-02 22:42:47 +0530

    Add new calender module

--- management/src/app/app.module.ts
+++ management/src/app/app.module.ts
@@ -1,117 +1,60 @@
 import {BrowserModule} from '@angular/platform-browser';
-
 import {NgModule} from '@angular/core';
 
-
-
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-
 import {AuthModule} from './core/auth/auth.module';
-
 import {AngularFireModule} from '@angular/fire';
-
 import {AngularFirestoreModule} from '@angular/fire/firestore';
-
 import {AngularFireAuthModule} from '@angular/fire/auth';
-
 import {environment} from '../environments/environment';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {AppRouteModule} from './route.module';
-
+import {LoginComponent} from './signup-login/login/login.component';
+import {SignupComponent} from './signup-login/signup/signup.component';
 import {UserSignInUpModule} from './signup-login/user-signin-up.module';
-
-import {ButtonsModule, CarouselModule, MDBBootstrapModule, NavbarModule, WavesModule} from 'angular-bootstrap-md';
-
 import {HomeComponent} from './home/home.component';
-
-import {MatButtonModule} from '@angular/material';
-
+import {DateAdapter, MatButtonModule} from '@angular/material';
+import {ButtonsModule, CarouselModule, MDBBootstrapModule, NavbarModule, WavesModule} from 'angular-bootstrap-md';
 import {UserProfileComponent} from './user-profile/user-profile.component';
-
 import {ContactComponent} from './contact-component/contact.component';
-
-import {CalendarModule, DateAdapter} from 'angular-calendar';
-
+import {CalendarModule} from 'angular-calendar';
 import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
-
 import {EventCalComponent} from './event-cal/event-cal.component';
-
-import {FormsModule} from '@angular/forms';
-
-import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
-
-import {FlatpickrModule} from 'angularx-flatpickr';
 
 
 
 @NgModule({
-
   declarations: [
-
     AppComponent,
-
     HomeComponent,
-
     UserProfileComponent,
-
     ContactComponent,
-
     EventCalComponent
-
   ],
-
   imports: [
-
     BrowserModule,
-
+    AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
-
-    AppRouteModule,
-
     AuthModule,
-
     AngularFireModule.initializeApp(environment.firebaseConfig),
-
     AngularFirestoreModule,
-
-    AngularFireAuthModule,
-
     UserSignInUpModule,
-
     MDBBootstrapModule.forRoot(),
-
     CarouselModule,
-
     WavesModule,
-
     ButtonsModule,
-
     MatButtonModule,
-
+    NavbarModule,
+    AngularFireAuthModule,
     NavbarModule,
 
-    FormsModule,
-
-    NgbModalModule,
-
-    FlatpickrModule.forRoot(),
-
     CalendarModule.forRoot({
-
       provide: DateAdapter,
-
       useFactory: adapterFactory
-
     })
-
   ],
-
   providers: [],
-
   bootstrap: [AppComponent]
-
 })
-
 export class AppModule { }
