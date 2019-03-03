3fe0b6aca54821561059be17b0f1e26806c81675
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2019-03-03 09:53:40 +0530

    Add module for services

---
+++ management/src/app/services/services.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PoolReservationService} from './pool-reservation.service';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [PoolReservationService]
+})
+
+export class ServicesModule {
+
+}
