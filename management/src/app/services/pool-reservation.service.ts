61eebc549f3aeab92e181aa91ff24abf77da90c0
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2019-03-03 16:18:14 +0530

    add method to save reservation

--- management/src/app/services/pool-reservation.service.ts
+++ management/src/app/services/pool-reservation.service.ts
@@ -1,17 +1,53 @@
 import {Injectable} from '@angular/core';
-import {AngularFirestore} from '@angular/fire/firestore';
+
+import {AngularFirestore, DocumentReference} from '@angular/fire/firestore';
+
+import {AuthService} from '../core/auth/auth.service';
+
+import {from, Observable, of} from 'rxjs';
+
+
 
 @Injectable({
+
   providedIn: 'root'
+
 })
+
 export class PoolReservationService {
-  constructor(public firestore: AngularFirestore) {
+
+
+
+  constructor(public firestore: AngularFirestore, public authService: AuthService) {
 
   }
 
-    //example fot save to firestore. this method can from any compponent
 
-    addEvent(event){
-      this.firestore.collection(`events/${event.month}`).add(event);
-    }
+
+  // example for save event to firestore. This method can called from any component
+
+  addReservationToUser(year, month, event): Observable<DocumentReference> {
+
+
+
+    let obs: Observable<DocumentReference>;
+
+    this.authService.user.subscribe(user => {
+
+      if (user != null) {
+
+        obs = from(this.firestore.collection(`user/${user.uid}/reservation/${year}/${month}`).add(event));
+
+      } else {
+
+        obs = of(null);
+
+      }
+
+    });
+
+    return obs;
+
+  }
+
 }
